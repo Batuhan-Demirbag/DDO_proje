7024cbf3fc566bafe2c5177353c1b5868bf3ed5f
Batuhan Demirbağ <batuhandemirbag.0623@gmail.com>
2024-02-09 17:45:46 +0300

    09.01
    Konuları alanında egzersiz yapılıdı (For , while , foreach , dowhile , array )

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Stirng dizisimdeki elemanların isimlerini yazdırma
+        var name = "batu";
+        string lastname = "demir";
+
+        var fullname = name + "" + lastname;
+        var myfullname = string.Format("My name {0} {1}\n",name , lastname);
+        Console.WriteLine(myfullname);
+        //output batu demir 
+
+        // ikinci yol
+        var names = new string[3] { "Volkan", "Biçen", "ddo" };
+        var mynames = string.Join(",", names);
+        Console.WriteLine(mynames);
+
+
+        // üçüncü yol 
+        string[] isimler = { "Arda", "Güler" ,"Ddo"};
+        foreach(string i in isimler)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
